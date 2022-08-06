357cd8fb060d79327384c9568b4494c315b08938
rahul-b91 <rahul.rohilla@card91.io>
2022-08-06 15:23:12 +0530

    fixed the api for checking followerId is following or not

--- index.js
+++ index.js
@@ -1,63 +1,70 @@
-require("./utils/axios_config")
+const { twitterAxios } = require("./utils/axios_config");
+const express = require("express");
+const { PORT } = process.env;
 
-const express = require('express')
-const {PORT} = process.env
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json()) 
-
-
-const {twitterAxios} = require('./utils/axios_config');
-
-
-app.get('/userDetail/:userName', async (req, res) => {
-    const {userName} = req.params;
-    const { data } = await twitterAxios.get(`/by/username/${userName}`)
+app.get("/userDetail/:userName", async (req, res) => {
+    const { userName } = req.params;
+    const { data } = await twitterAxios.get(`/by/username/${userName}`);
     res.send(data);
-})
+});
 
-app.get('/followers/:userId', async (req, res) => {
-    const {userId} = req.params;
-    const { data } = await twitterAxios.get(`/${userId}/followers`)
+app.get("/followers/:userId", async (req, res) => {
+    const { userId } = req.params;
+    const { data } = await twitterAxios.get(`/${userId}/followers`);
     res.send(data);
-})
+});
 
+app.get("/isFollowing", async (req, res) => {
+    const { userIdToFollow, userIdIsFollowing } = req.body;
+    const {
+        data: { data: followers },
+    } = await twitterAxios.get(`/${userIdToFollow}/followers`);
 
-app.get('/followersMatching', async (req, res) => {
-    const {userId1, userId2} = req.body;
-    const { data } = await twitterAxios.get(`/${1555623173605183488}/follower`)
-    res.send(data);
-})
-
-app.get('/', async (req, res) => {
-    res.send({
-        apiList: [{
-            getUserDetails: {
-                url: '/userDetail/:userName',
-                description: 'Get the user details based on the userName',
-                requestBody: {},
-                status: 'active'
-            },
-            getUserFollowersById: {
-                url: '/followers/:userId',
-                requestBody: {},
-                description: 'Get the user followers based on the userId',
-                status: 'active'
-            },
-            followersMatching: {
-                url: '/followersMatching',
-                status: 'in-active',
-                description: 'Get the user followers based on the userId',
-                requestBody: {
-                    userId1: "",
-                    userId2: ""
-                }
-            }
-        }]
+    const isFollowing = followers.find((follower) => {
+        return follower.id === userIdIsFollowing;
     });
-})
 
+    const descAboutFollowingStatus = `${userIdIsFollowing} is ${
+        isFollowing ? "following" : "not following"
+    } to ${userIdToFollow}`;
+    res.send({
+        status: descAboutFollowingStatus,
+    });
+});
+
+app.get("/", async (req, res) => {
+    res.send({
+        apiList: [
+            {
+                getUserDetails: {
+                    url: "/userDetail/:userName",
+                    description: "Get the user details based on the userName",
+                    requestBody: {},
+                    status: "active",
+                },
+                getUserFollowersById: {
+                    url: "/followers/:userId",
+                    requestBody: {},
+                    description: "Get the user followers based on the userId",
+                    status: "active",
+                },
+                followersMatching: {
+                    url: "/followersMatching",
+                    status: "active",
+                    description: "Get the user followers based on the userId",
+                    requestBody: {
+                        userId1: "",
+                        userId2: "",
+                    },
+                },
+            },
+        ],
+    });
+});
 
 app.listen(PORT, () => {
-    console.log("Server is started")
-})
+    console.log("Server is started");
+});
